52d4de4146fef8f93921dd665c5419f85fe1318e
guilhermemtgama <101999593+guilhermemtgama@users.noreply.github.com>
2023-12-13 23:29:38 -0300

    Create Controle de estrutura If

---
+++ Projetos Js/Controle de Estrutura/controleDeEstruturaIF.js
@@ -0,0 +1,9 @@
+let temperature = 10;
+
+if (temperature > 30) {
+  console.log("It's a hot day!");
+} else if (temperature >= 20 && temperature <= 30) {
+  console.log("The weather is pleasant.");
+} else {
+  console.log("It's a cold day!");
+}
