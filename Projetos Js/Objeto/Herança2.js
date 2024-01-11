b8b167fd2896e1b35f9ec92b22f6565e6ec985ae
guilhermemtgama <101999593+guilhermemtgama@users.noreply.github.com>
2024-01-10 21:26:21 -0300

    Tipo 2 de Herança em JS

---
+++ Projetos Js/Objeto/Herança2.js
@@ -0,0 +1,43 @@
+// Cadeia de protótipos (Prototype chain)
+const avo = {attr1: "A"}
+const pai = {__proto__:avo,attr2: "B"}
+const filho = {__proto__:pai,attr3: "C"}
+console.log(filho.attr1, filho.attr2, filho.attr3)
+
+const carro = {
+    velAtual:0,
+    velMax: 200,
+    aceleraMais(delta) {
+        if (this.velAtual + delta <= this.velMax){
+            this.velAtual += delta
+        }else{
+            this.velAtual = this.velMax
+        }
+    },
+    status() {
+        return `${this.velAtual}Km/h de ${this.velMax}Km/h`
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 325 //shadowing    
+}
+
+const volvo = {
+    modelo: 'V40',
+    status(){
+        return `${this.modelo}: ${super.status()}`
+    }
+}
+
+Object.setPrototypeOf(ferrari,carro)
+Object.setPrototypeOf(volvo,carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.aceleraMais(100)
+console.log(volvo.status())
+ferrari.aceleraMais()
+console.log(ferrari.status())
